d032676f271a713f764839b1ced577e111bbcab6
shivanshu_gupta1 <shivanshu981@gmail.com>
2021-05-31 14:40:33 +0530

    Work

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,95 @@
-import React from 'react'
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+
+
+const useStyles = makeStyles({
+    list: {
+      width: 250,
+    },
+    fullList: {
+      width: 'auto',
+    },
+});
+
 
 const Header = () => {
+
+    const classes = useStyles();
+    const [state, setState] = React.useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+    });
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+          return;
+        }
+    
+        setState({ ...state, [anchor]: open });
+    };
+
+    const list = (anchor) => (
+        <div
+          className={clsx(classes.list, {
+            [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+          })}
+          role="presentation"
+          onClick={toggleDrawer(anchor, false)}
+          onKeyDown={toggleDrawer(anchor, false)}
+        >
+          <List>
+            <ListItem button onClick={() => window.location.href = "/emailverification"} >
+                <ListItemText primary={'Email Verification'} />
+            </ListItem>
+            <ListItem button onClick={() => window.location.href = "/"} >
+                <ListItemText primary={'Email Sending'} />
+            </ListItem>
+            <ListItem button onClick={() => window.location.href = "/"} >
+                <ListItemText primary={'Email Templates'} />
+            </ListItem>
+            <ListItem button onClick={() => window.location.href = "/"} >
+                <ListItemText primary={'About'} />
+            </ListItem>
+            <ListItem button onClick={() => window.location.href = "/pricing"} >
+                <ListItemText primary={'Pricing'} />
+            </ListItem>
+            <ListItem button onClick={() => window.location.href = "/contact"} >
+                <ListItemText primary={'Contact Us'} />
+            </ListItem>
+            <ListItem button onClick={() => window.location.href = "/signin"} >
+                <ListItemText primary={'Go to Console'} />
+            </ListItem>
+          </List>
+        </div>
+    );
+
+
     return (
         <>
         <nav class="navbar navbar-expand-lg navbar-light bg-white">
             <div style={{ maxWidth: '1900px' }} class="container-lg">
-                <a class="navbar-brand d-lg-none" href="index.html">Marketing Platform</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
+                <a class="navbar-brand d-lg-none" href="/">Marketing Platform</a>
+                {['left'].map((anchor) => (
+                    <React.Fragment key={anchor}>
+                    <button class="navbar-toggler" type="button" onClick={toggleDrawer(anchor, true)} >
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+                        {list(anchor)}
+                    </Drawer>
+                    </React.Fragment>
+                ))}
+                
+
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav justify-content-end w-100">
                     <li class="nav-item dropdown">
